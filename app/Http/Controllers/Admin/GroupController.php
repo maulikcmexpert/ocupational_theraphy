61c65c16b5c35959c7597613269772202e4f22df
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-05-08 18:38:40 +0530

    Update GroupController.php

--- app/Http/Controllers/Admin/GroupController.php
+++ app/Http/Controllers/Admin/GroupController.php
@@ -219,8 +219,7 @@ class GroupController extends CoreController
                 $sessionDate = [];
 
                 for ($session = 0; $session < $total_session; $session++) {
-                    echo $session;
-                    exit;
+
                     // Calculate the session date based on the start date and session number
                     $session_date = $start_session_date->copy()->addDays($session * 7);
 

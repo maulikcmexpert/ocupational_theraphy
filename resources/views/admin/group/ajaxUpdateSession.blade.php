2e83287a48fcee6606b276375dc12781d3fa9ced
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-05-09 11:07:19 +0530

    Update ajaxUpdateSession.blade.php

--- resources/views/admin/group/ajaxUpdateSession.blade.php
+++ resources/views/admin/group/ajaxUpdateSession.blade.php
@@ -28,7 +28,8 @@ $totalInsertedSession = count($group_session);
     ?>
     @if($newSession > 0)
     @for($i=1;$i<=$newSession;$i++) <div class="col-12 col-xxl-4 col-xl-4 col-lg-6 col-md-6 col-sm-6 position-relative">
-        <label class="required fw-bold fs-6 mb-2">Session name</label>
+        <label class="required fw-bold fs-6 mb-2">Sessions name</label>
+
         <input type="text" name="session_name[]" class="form-control session_name">
         <span class="availdocerror"></span>
         <span class=" sessionremove"><i class="fa fa-close"></i></span>

fea7545e40b86908b9072585cb0beca09048e468
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-05-09 19:21:35 +0530

    Update ajaxUpdateSession.blade.php

--- resources/views/admin/group/ajaxUpdateSession.blade.php
+++ resources/views/admin/group/ajaxUpdateSession.blade.php
@@ -52,6 +52,8 @@ $totalInsertedSession = count($group_session);
 
         <?php
         $j = 0;
+        $newSession = $totalSession - $totalInsertedSession;
+        dd($newSession);
         ?>
         @for($i=1;$i<=$totalSession;$i++) <div class="col-12 col-xxl-4 col-xl-4 col-lg-6 col-md-6 col-sm-6 position-relative">
             <label class="required fw-bold fs-6 mb-2">Session name</label>

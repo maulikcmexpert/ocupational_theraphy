b07cd7081f776339faa439d9f4f4fd95fa87a588
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-05-09 12:25:57 +0530

    Update ajaxUpdateSession.blade.php

--- resources/views/admin/group/ajaxUpdateSession.blade.php
+++ resources/views/admin/group/ajaxUpdateSession.blade.php
@@ -30,7 +30,7 @@ $totalInsertedSession = count($group_session);
     @for($i=1;$i<=$newSession;$i++) <div class="col-12 col-xxl-4 col-xl-4 col-lg-6 col-md-6 col-sm-6 position-relative">
         <label class="required fw-bold fs-6 mb-2">Session name</label>
         <input type="date" name="session_date[]" value="{{ $newSessionDate[$i-1]}}" class="form-control session_date">
-        <input type="text" name="session_name[]" value="{{ Session.' '.$totalInsertedSession+1}}" class="form-control session_name">
+        <input type="text" name="session_name[]" value="{{ 'Session '.$totalInsertedSession+1}}" class="form-control session_name">
         <span class="availdocerror"></span>
         <span class=" sessionremove"><i class="fa fa-close"></i></span>
 

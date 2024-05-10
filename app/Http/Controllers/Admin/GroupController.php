7702319667af4112e9c40915e3931e51c9f1c7f9
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-05-10 12:08:39 +0530

    Update GroupController.php

--- app/Http/Controllers/Admin/GroupController.php
+++ app/Http/Controllers/Admin/GroupController.php
@@ -745,8 +745,8 @@ class GroupController extends CoreController
         $sessionId = $request->input('sessionId');
 
         // Query the database to check if the selected date already exists
-        $exists = Group_session::where('session_date', $selectedDate)->exists();
-        dd($exists);
+        $exists = Group_session::where(['session_date' => $selectedDate])->where('id', '!=', $sessionId)->exists();
+
         return response()->json(['exists' => $exists]);
     }
 }

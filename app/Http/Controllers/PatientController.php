bb1563a5cb1a7a5f949738c226e38fb01843fe79
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-05-13 19:44:25 +0530

    Update PatientController.php

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -1358,9 +1358,9 @@ class PatientController extends Controller
             $patientName = str_replace(' ', '_', $patientData->first_name) . '_' . str_replace(' ', '_', $patientData->last_name);
             $filename = $patientName . '.pdf';
 
-            $tempPath = storage_path('app/' . $filename);
+            $tempPath = storage_path('public/storage/pdf/' . $filename);
             $pdf->save($tempPath);
-            $destinationPath = public_path('storage/pdf/');
+            $destinationPath = public_path('public/storage/pdf/');
             \File::move($tempPath, $destinationPath . $filename);
 
 

9564b3b36165ddd6f4c60cc737ac7d69001be31b
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-05-27 19:14:56 +0530

    Update consentform.blade.php

--- resources/views/patient/consentform.blade.php
+++ resources/views/patient/consentform.blade.php
@@ -13,111 +13,236 @@
                             <h3>Identification of person giving consent</h3>
                         </div>
                         <div class="main-content">
-
-                            <?php foreach ($question as $key => $val) {
-                                if ($key == 5) {
-                                    break;
-                                }
-                            ?>
-                                <div class="form-check">
-                                    <div>
-                                        <label class="form-check-label" for="flexCheckDefault">
-                                            {{ $val->question }}
-
-                                            <?php if ($val->ques_type == 'text') { ?>
-                                                <input class="form-control" type="hidden" name="question_id[]" value="{{ $val->id }}">
-                                                <input class="form-control" type="text" name="answer[]">
-                                            <?php } ?>
-                                        </label>
-                                    </div>
-
-                                    <div>
-                                        <?php if ($val->ques_type == 'check') { ?>
-                                            <input class="form-control" type="hidden" name="question_id[]" value="{{ $val->id }}">
-                                            <input class="form-control" type="hidden" name="answer[{{ $val->id }}]" value="0">
-                                            <input class="form-check-input" name="answer[{{ $val->id }}]" type="checkbox" value="1" id="flexCheckDefault">
-                                        <?php } ?>
-                                    </div>
+                            @foreach ($question as $key => $val)
+                            @if ($key == 5)
+                            @break
+                            @endif
+                            <div class="form-check">
+                                <div>
+                                    <label class="form-check-label" for="flexCheckDefault_{{ $val->id }}">
+                                        {{ $val->question }}
+                                        @if ($val->ques_type == 'text')
+                                        <input class="form-control" type="hidden" name="question_id[]" value="{{ $val->id }}">
+                                        <input class="form-control" type="text" name="answer[]">
+                                        @endif
+                                    </label>
                                 </div>
-                            <?php } ?>
+                                <div>
+                                    @if ($val->ques_type == 'check')
+                                    <input type="hidden" name="question_id[]" value="{{ $val->id }}">
+                                    <input type="hidden" name="answer[]" value="0">
+                                    <input class="form-check-input" name="answer[]" type="checkbox" value="1" id="flexCheckDefault_{{ $val->id }}">
+                                    @endif
+                                </div>
+                            </div>
+                            @endforeach
                         </div>
                     </div>
                 </div>
-
-                <!-- Repeat the structure for other sections -->
-
                 <div class="col-12 mb-3">
                     <div class="main-content-wrp">
                         <div class="main-content-title">
                             <h3>Informed Consent: Assessment And Treatment</h3>
                         </div>
                         <div class="main-content">
-                            <?php foreach ($question as $key => $val) {
-                                if ($key >= 5 && $key <= 8) {
-                            ?>
-                                    <div class="form-check">
-                                        <div>
-                                            <label class="form-check-label" for="flexCheckDefault">
-                                                {{ $val->question }}
-                                                <?php if ($val->ques_type == 'text') { ?>
-                                                    <input class="form-control" type="hidden" name="question_id[]" value="{{ $val->id }}">
-                                                    <input class="form-control" type="text" name="answer[]">
-                                                <?php } ?>
-                                            </label>
-                                        </div>
-                                        <div>
-                                            <?php if ($val->ques_type == 'check') { ?>
-                                                <input class="form-control" type="hidden" name="question_id[]" value="{{ $val->id }}">
-                                                <input class="form-control" type="hidden" name="answer[{{ $val->id }}]" value="0">
-                                                <input class="form-check-input" name="answer[{{ $val->id }}]" type="checkbox" value="1" id="flexCheckDefault">
-                                            <?php } ?>
-                                        </div>
-                                    </div>
-                            <?php }
-                            } ?>
+                            @foreach ($question as $key => $val)
+                            @if ($key >= 5 && $key <= 8) <div class="form-check">
+                                <div>
+                                    <label class="form-check-label" for="flexCheckDefault_{{ $val->id }}">
+                                        {{ $val->question }}
+                                        @if ($val->ques_type == 'text')
+                                        <input class="form-control" type="hidden" name="question_id[]" value="{{ $val->id }}">
+                                        <input class="form-control" type="text" name="answer[]">
+                                        @endif
+                                    </label>
+                                </div>
+                                <div>
+                                    @if ($val->ques_type == 'check')
+                                    <input type="hidden" name="question_id[]" value="{{ $val->id }}">
+                                    <input type="hidden" name="answer[]" value="0">
+                                    <input class="form-check-input" name="answer[]" type="checkbox" value="1" id="flexCheckDefault_{{ $val->id }}">
+                                    @endif
+                                </div>
                         </div>
+                        @endif
+                        @endforeach
                     </div>
                 </div>
-
-                <!-- Continue with the rest of the sections similarly -->
-
-                <div class="col-12 mb-3">
-                    <div class="main-content-wrp">
-                        <div class="main-content-title">
-                            <h3>Withdrawal of Consent</h3>
-                        </div>
-                        <div class="main-content">
-                            <?php foreach ($question as $key => $val) {
-                                if ($key >= 9 && $key <= 10) {
-                            ?>
-                                    <div class="form-check">
-                                        <div>
-                                            <label class="form-check-label" for="flexCheckDefault">
-                                                {{ $val->question }}
-                                                <?php if ($val->ques_type == 'text') { ?>
-                                                    <input class="form-control" type="hidden" name="question_id[]" value="{{ $val->id }}">
-                                                    <input class="form-control" type="text" name="answer[]">
-                                                <?php } ?>
-                                            </label>
-                                        </div>
-                                        <div>
-                                            <?php if ($val->ques_type == 'check') { ?>
-                                                <input class="form-control" type="hidden" name="question_id[]" value="{{ $val->id }}">
-                                                <input class="form-control" type="hidden" name="answer[{{ $val->id }}]" value="0">
-                                                <input class="form-check-input" name="answer[{{ $val->id }}]" type="checkbox" value="1" id="flexCheckDefault">
-                                            <?php } ?>
-                                        </div>
-                                    </div>
-                            <?php }
-                            } ?>
-                        </div>
-                    </div>
-                </div>
-
-                <!-- Continue with the rest of the sections similarly -->
-
             </div>
-            <input class="btn btn-primary" type="submit" value="Submit">
-        </form>
+            <div class="col-12 mb-3">
+                <div class="main-content-wrp">
+                    <div class="main-content-title">
+                        <h3>Withdrawal of Consent</h3>
+                    </div>
+                    <div class="main-content">
+                        @foreach ($question as $key => $val)
+                        @if ($key >= 9 && $key <= 10) <div class="form-check">
+                            <div>
+                                <label class="form-check-label" for="flexCheckDefault_{{ $val->id }}">
+                                    {{ $val->question }}
+                                    @if ($val->ques_type == 'text')
+                                    <input class="form-control" type="hidden" name="question_id[]" value="{{ $val->id }}">
+                                    <input class="form-control" type="text" name="answer[]">
+                                    @endif
+                                </label>
+                            </div>
+                            <div>
+                                @if ($val->ques_type == 'check')
+                                <input type="hidden" name="question_id[]" value="{{ $val->id }}">
+                                <input type="hidden" name="answer[]" value="0">
+                                <input class="form-check-input" name="answer[]" type="checkbox" value="1" id="flexCheckDefault_{{ $val->id }}">
+                                @endif
+                            </div>
+                    </div>
+                    @endif
+                    @endforeach
+                </div>
+            </div>
     </div>
+    <div class="col-12 mb-3">
+        <div class="main-content-wrp">
+            <div class="main-content-title">
+                <h3>Electronic Communication:</h3>
+            </div>
+            <div class="main-content">
+                @foreach ($question as $key => $val)
+                @if ($key >= 11 && $key <= 15) <div class="form-check">
+                    <div>
+                        <label class="form-check-label" for="flexCheckDefault_{{ $val->id }}">
+                            {{ $val->question }}
+                            @if ($val->ques_type == 'text')
+                            <input class="form-control" type="hidden" name="question_id[]" value="{{ $val->id }}">
+                            <input class="form-control" type="text" name="answer[]">
+                            @endif
+                        </label>
+                    </div>
+                    <div>
+                        @if ($val->ques_type == 'check')
+                        <input type="hidden" name="question_id[]" value="{{ $val->id }}">
+                        <input type="hidden" name="answer[]" value="0">
+                        <input class="form-check-input" name="answer[]" type="checkbox" value="1" id="flexCheckDefault_{{ $val->id }}">
+                        @endif
+                    </div>
+            </div>
+            @endif
+            @endforeach
+        </div>
+    </div>
+</div>
+<div class="col-12 mb-3">
+    <div class="main-content-wrp">
+        <div class="main-content-title">
+            <h3>Telehealth Appointments</h3>
+        </div>
+        <div class="main-content">
+            @foreach ($question as $key => $val)
+            @if ($key == 16)
+            <div class="form-check">
+                <div>
+                    <label class="form-check-label" for="flexCheckDefault_{{ $val->id }}">
+                        {{ $val->question }}
+                        @if ($val->ques_type == 'text')
+                        <input class="form-control" type="hidden" name="question_id[]" value="{{ $val->id }}">
+                        <input class="form-control" type="text" name="answer[]">
+                        @endif
+                    </label>
+                </div>
+                <div>
+                    @if ($val->ques_type == 'check')
+                    <input type="hidden" name="question_id[]" value="{{ $val->id }}">
+                    <input type="hidden" name="answer[]" value="0">
+                    <input class="form-check-input" name="answer[]" type="checkbox" value="1" id="flexCheckDefault_{{ $val->id }}">
+                    @endif
+                </div>
+            </div>
+            @endif
+            @endforeach
+        </div>
+    </div>
+</div>
+<div class="col-12 mb-3">
+    <div class="main-content-wrp">
+        <div class="main-content-title">
+            <h3>Billing consent:</h3>
+        </div>
+        <div class="main-content">
+            @foreach ($question as $key => $val)
+            @if ($key >= 17 && $key <= 21) <div class="form-check">
+                <div>
+                    <label class="form-check-label" for="flexCheckDefault_{{ $val->id }}">
+                        {{ $val->question }}
+                        @if ($val->ques_type == 'text')
+                        <input class="form-control" type="hidden" name="question_id[]" value="{{ $val->id }}">
+                        <input class="form-control" type="text" name="answer[]">
+                        @endif
+                    </label>
+                </div>
+                <div>
+                    @if ($val->ques_type == 'check')
+                    <input type="hidden" name="question_id[]" value="{{ $val->id }}">
+                    <input type="hidden" name="answer[]" value="0">
+                    <input class="form-check-input" name="answer[]" type="checkbox" value="1" id="flexCheckDefault_{{ $val->id }}">
+                    @endif
+                </div>
+        </div>
+        @endif
+        @endforeach
+    </div>
+</div>
+</div>
+<div class="col-12 mb-3">
+    <div class="main-content-wrp">
+        <div class="main-content-title">
+            <h3>PROTECTION OF PERSONAL INFORMATION AT THIS PRACTICE</h3>
+            <h5>(POPI and PAIA manuals are available at www.groundedwellwise.co.za)</h5>
+        </div>
+        <div class="main-content">
+            @foreach ($question as $key => $val)
+            @if ($key >= 22)
+            <div class="form-check">
+                <div>
+                    <label class="form-check-label" for="flexCheckDefault_{{ $val->id }}">
+                        {{ $val->question }}
+                        @if ($val->ques_type == 'text')
+                        <input class="form-control" type="hidden" name="question_id[]" value="{{ $val->id }}">
+                        <input class="form-control" type="text" name="answer[]">
+                        @endif
+                    </label>
+                </div>
+                <div>
+                    @if ($val->ques_type == 'check')
+                    <input type="hidden" name="question_id[]" value="{{ $val->id }}">
+                    <input type="hidden" name="answer[]" value="0">
+                    <input class="form-check-input" name="answer[]" type="checkbox" value="1" id="flexCheckDefault_{{ $val->id }}">
+                    @endif
+                </div>
+            </div>
+            @if ($val->ques_type == 'text_two')
+            <input class="form-control" type="hidden" name="question_id[]" value="{{ $val->id }}">
+            <div class="form-check">
+                <div>
+                    <label class="form-check-label" for="flexCheckDefault_{{ $val->id }}">
+                        My treating team, namely: <input class="form-control" type="text" name="answer[]">
+                    </label>
+                </div>
+                <div></div>
+            </div>
+            <div class="form-check">
+                <div>
+                    <label class="form-check-label" for="flexCheckDefault_{{ $val->id }}">
+                        Family members, namely: <input class="form-control" type="text" name="answer[]">
+                    </label>
+                </div>
+                <div></div>
+            </div>
+            @endif
+            @endif
+            @endforeach
+        </div>
+    </div>
+</div>
+</div>
+<input class="btn btn-primary" type="submit" value="Submit">
+</form>
+</div>
 </div>

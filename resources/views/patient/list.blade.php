86c988cfeea2aec0ea92df4f3fdea49ebdef45f0
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-05-27 15:28:54 +0530

    Update list.blade.php

--- resources/views/patient/list.blade.php
+++ resources/views/patient/list.blade.php
@@ -83,6 +83,20 @@
                             <!--end::Add user-->
                         </div>
 
+                        <div class="d-flex justify-content-end" data-kt-user-table-toolbar="base">
+
+                            <!--begin::Add user-->
+                            <a href="{{route('patient.consentForm')}}" class="btn add-patient text-white">
+                                <!--begin::Svg Icon | path: icons/duotune/arrows/arr075.svg-->
+                                <span class="svg-icon svg-icon-2">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                        <rect opacity="0.5" x="11.364" y="20.364" width="16" height="2" rx="1" transform="rotate(-90 11.364 20.364)" fill="black" />
+                                        <rect x="4.36396" y="11.364" width="16" height="2" rx="1" fill="black" />
+                                    </svg>
+                                </span>
+                                <!--end::Svg Icon-->ConsentForm</a>
+                            <!--end::Add user-->
+                        </div>
 
                     </div>
                     <!--end::Card toolbar-->

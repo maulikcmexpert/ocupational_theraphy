c7b2c549ca6c5d3e8235668dff1f4515f3ac9175
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-05-27 20:23:53 +0530

    Update PatientController.php

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -239,6 +239,7 @@ class PatientController extends Controller
         $patientId = decrypt($id);
 
         foreach ($request->questions as $val) {
+            dd($val->question);
             $answer = new ConsentAnswer();
             $answer->patient_id = $patientId;
             $answer->question_id = $val->question;

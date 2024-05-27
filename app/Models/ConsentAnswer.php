169272bbfc84f62fee205bf6b37544631b601f96
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-05-27 18:17:50 +0530

    c

---
+++ app/Models/ConsentAnswer.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ConsentAnswer extends Model
+{
+    use HasFactory;
+}

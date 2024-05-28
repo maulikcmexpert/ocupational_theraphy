1bcb6456e80ac73b4fb0bf2a0a596fb93f909aa0
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-05-28 20:03:22 +0530

    c

--- public/assets/admin/js/consentform.js
+++ public/assets/admin/js/consentform.js
@@ -1,77 +1,31 @@
-$("#consentForm").validate({
-  rules: {
-    avatar: { required: true, accept: "image/*" },
-    first_name: { required: true },
-    last_name: { required: true },
-    date_of_birth: { required: true },
-    profession: { required: true },
-    email: {
-      required: true,
-      email: true,
-      remote: {
-        headers: {
-          "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
-        },
-        url: base_url + "admin/doctor/check_email_is_already",
-        method: "POST",
-        data: {
-          email: function () {
-            return $("input[name='email']").val();
-          },
-        },
+$(document).ready(function () {
+  // Add a custom validation method for checkbox
+  $.validator.addMethod(
+    "checkboxRequired",
+    function (value, element) {
+      return $(element).is(":checked");
+    },
+    "Please fill atleast one."
+  );
+
+  // Initialize jQuery Validate plugin
+  $("#myForm").validate({
+    rules: {
+      "questions[0][answer]": {
+        checkboxRequired: true,
       },
     },
-    identity_number: {
-      required: true,
-      number: true,
-      minlength: 13,
-      maxlength: 13,
-      remote: {
-        headers: {
-          "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
-        },
-        url: base_url + "admin/doctor/check_identity_number_is_already",
-        method: "POST",
-        data: {
-          identity_number: function () {
-            return $("input[name='identity_number']").val();
-          },
-        },
+    messages: {
+      "questions[0][answer]": {
+        checkboxRequired: "Please fill atleast one.",
       },
     },
-    password: { required: true, min: 6 },
-    gender: { required: true },
-    contact_number: { required: true, number: true },
-  },
-  messages: {
-    avatar: {
-      required: "Please upload avatar",
-      accept: "Please upload avtar in jpg,png,jpeg format",
+    errorPlacement: function (error, element) {
+      if (element.attr("type") === "checkbox") {
+        error.insertAfter(element.next("label"));
+      } else {
+        error.insertAfter(element);
+      }
     },
-    first_name: { required: "Please enter first name" },
-    last_name: { required: "Please enter last name" },
-    profession: { required: "Please enter profession" },
-    date_of_birth: { required: "Please select date of birth" },
-    email: {
-      required: "Please enter email",
-      email: "Please enter vaild email",
-      remote: "Email is already exist",
-    },
-    identity_number: {
-      required: "Please enter identity number",
-      number: "Identity number must be only digit",
-      minlength: "Please enter 13 digit Identity number",
-      maxlength: "Please enter 13 digit Identity number",
-      remote: "Identity number is already exist",
-    },
-    password: {
-      required: "Please enter password",
-      min: "Password must be minimum 6 and more character",
-    },
-    gender: { required: "Please enter gender" },
-    contact_number: {
-      required: "Please enter contact number",
-      number: "Please enter only digit",
-    },
-  },
+  });
 });

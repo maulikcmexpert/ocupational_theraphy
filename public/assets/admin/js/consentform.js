da4aa05910a90820f77e002b78c1135181ab8484
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-05-28 20:28:37 +0530

    Update consentform.js

--- public/assets/admin/js/consentform.js
+++ public/assets/admin/js/consentform.js
@@ -9,21 +9,28 @@ $(document).ready(function () {
   );
 
   // Initialize jQuery Validate plugin
-  var validationRules = {};
-  var validationMessages = {};
-  for (var i = 0; i <= 34; i++) {
-    var fieldName = "questions[" + i + "][answer]";
-    validationRules[fieldName] = {
-      atLeastOneCheckbox: true,
-    };
-    validationMessages[fieldName] = {
-      atLeastOneCheckbox:
-        "Please select at least one checkbox for question " + (i + 1),
-    };
-  }
-
   $("#consentForm").validate({
-    rules: validationRules,
-    messages: validationMessages,
+    rules: {
+      // Assuming checkboxes have name attribute as questions[i][answer][]
+      // where i ranges from 0 to 34
+      "questions[][answer]": {
+        atLeastOneCheckbox: true,
+      },
+    },
+    messages: {
+      "questions[][answer]": {
+        atLeastOneCheckbox:
+          "Please select at least one checkbox for this question.",
+      },
+    },
+    submitHandler: function (form) {
+      // Check if at least one checkbox is checked before form submission
+      if ($("input[name^='questions'][type='checkbox']:checked").length === 0) {
+        alert("Please select at least one checkbox.");
+        return false; // Prevent form submission
+      } else {
+        form.submit(); // Submit the form if at least one checkbox is checked
+      }
+    },
   });
 });

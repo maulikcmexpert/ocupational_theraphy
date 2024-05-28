a7ef9336ffbe5827e7b839682419922b70b9a2b0
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-05-28 20:13:19 +0530

    Update consentform.js

--- public/assets/admin/js/consentform.js
+++ public/assets/admin/js/consentform.js
@@ -1,31 +1,17 @@
 $(document).ready(function () {
   // Add a custom validation method for checkbox
-  $.validator.addMethod(
-    "checkboxRequired",
-    function (value, element) {
-      return $(element).is(":checked");
-    },
-    "Please fill atleast one."
-  );
 
   // Initialize jQuery Validate plugin
   $("#consentForm").validate({
     rules: {
       "questions[0][answer]": {
-        checkboxRequired: true,
+        required: true,
       },
     },
     messages: {
       "questions[0][answer]": {
-        checkboxRequired: "Please fill atleast one.",
+        required: "Please fill atleast one.",
       },
-    },
-    errorPlacement: function (error, element) {
-      if (element.attr("type") === "checkbox") {
-        error.insertAfter(element.next("label"));
-      } else {
-        error.insertAfter(element);
-      }
     },
   });
 });

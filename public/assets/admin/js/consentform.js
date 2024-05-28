ab81e4eea1d1c5d5260c5e8a363f66557d725b04
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-05-28 20:24:50 +0530

    Update consentform.js

--- public/assets/admin/js/consentform.js
+++ public/assets/admin/js/consentform.js
@@ -1,17 +1,29 @@
 $(document).ready(function () {
   // Add a custom validation method for checkbox
+  $.validator.addMethod(
+    "atLeastOneCheckbox",
+    function (value, element) {
+      return $(element).is(":checked");
+    },
+    "Please select at least one checkbox."
+  );
 
   // Initialize jQuery Validate plugin
+  var validationRules = {};
+  var validationMessages = {};
+  for (var i = 0; i <= 34; i++) {
+    var fieldName = "questions[" + i + "][answer][]";
+    validationRules[fieldName] = {
+      atLeastOneCheckbox: true,
+    };
+    validationMessages[fieldName] = {
+      atLeastOneCheckbox:
+        "Please select at least one checkbox for question " + (i + 1),
+    };
+  }
+
   $("#consentForm").validate({
-    rules: {
-      "questions[0][answer]": {
-        required: true,
-      },
-    },
-    messages: {
-      "questions[0][answer]": {
-        required: "Please fill atleast one.",
-      },
-    },
+    rules: validationRules,
+    messages: validationMessages,
   });
 });

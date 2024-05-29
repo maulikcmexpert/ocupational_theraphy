d8dfe61e9fd534cc9c50555214d2d3500c5b7632
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-05-29 13:17:04 +0530

    Update consentform.js

--- public/assets/admin/js/consentform.js
+++ public/assets/admin/js/consentform.js
@@ -1,35 +1,23 @@
 $(document).ready(function () {
-  // Add a custom validation method for checkbox
-  $.validator.addMethod(
-    "atLeastOneCheckbox",
-    function (value, element) {
-      return $(element).is(":checked");
-    },
-    "Please select at least one checkbox."
-  );
-
-  // Initialize jQuery Validate plugin
   $("#consentForm").validate({
     rules: {
-      // Assuming checkboxes have name attribute as questions[i][answer][]
-      // where i ranges from 0 to 34
-      "questions[][answer]": {
+      "questions[0][answer]": {
+        required: true,
+      },
+      "questions[1][answer]": {
+        required: true,
+      },
+      "questions[2][answer]": {
         required: true,
       },
     },
     messages: {
-      "questions[][answer]": {
-        required: "Please select at least one checkbox for this question.",
+      "questions[0][answer]": {
+        required: "Please select atleast one",
       },
     },
     submitHandler: function (form) {
-      // Check if at least one checkbox is checked before form submission
-      if ($("input[name^='questions'][type='checkbox']:checked").length === 0) {
-        $("label .error").html("Please select at least one.");
-        return false; // Prevent form submission
-      } else {
-        form.submit(); // Submit the form if at least one checkbox is checked
-      }
+      form.submit();
     },
   });
 });

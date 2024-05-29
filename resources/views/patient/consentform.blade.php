77c1b83dff33a4c42179b30fb673e43d7bc3f67b
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-05-29 11:49:23 +0530

    Update consentform.blade.php

--- resources/views/patient/consentform.blade.php
+++ resources/views/patient/consentform.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="1width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset('public/assets/css/custom.css')}}">
     <link rel="stylesheet" href="{{asset('public/assets/css/consent.css')}}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>General Consent Form for GWW APP</title>

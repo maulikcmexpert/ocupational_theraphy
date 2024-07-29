a53541a9dab4429ab45b4ea993d3c4e50d0b6a31
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-07-29 18:59:42 +0530

    Update consentform.js

--- public/assets/admin/js/consentform.js
+++ public/assets/admin/js/consentform.js
@@ -18,34 +18,15 @@
 
 $(document).ready(function () {
   $("#consentForm").on("submit", function (event) {
-    // if ($('input[type="checkbox"]:checked').length === 0) {
-    //   $(".consentFormError").html("Please select at least one option.");
-    //   $('input[type="checkbox"]').css("outline", "2px solid red"); // Highlight checkboxes
-    //   event.preventDefault();
-    // } else {
-    //   $('input[type="checkbox"]').css("outline", ""); // Remove highlight if validation passes
-    // }
-
-    var checkboxGroups = $(".form-check");
-    var valid = false;
-
-    // Loop through each group
-    checkboxGroups.each(function () {
-      // Check if any checkbox in the current group is checked
-      if ($(this).find('input[type="checkbox"]').is(":checked")) {
-        valid = true;
-        return false; // Break out of the loop if one group is valid
-      }
-    });
-
-    // If no checkbox is checked in any group, prevent form submission
-    if (!valid) {
-      event.preventDefault(); // Prevent form submission
-      alert(
-        "Please check at least one checkbox in each section before submitting."
-      );
-    }
+    var checkboxes = $(this).find(".form-check-input");
+    alert(checkboxes);
     event.preventDefault();
+    if ($('input[type="checkbox"]:checked').length === 0) {
+      $(".consentFormError").html("Please select at least one option.");
+      $('input[type="checkbox"]').css("outline", "2px solid red"); // Highlight checkboxes
+    } else {
+      $('input[type="checkbox"]').css("outline", ""); // Remove highlight if validation passes
+    }
   });
 
   // Remove the outline when a checkbox is checked

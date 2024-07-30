dcf88b882b1528bc4df1710c53a04ecdc8ab514f
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-07-30 16:57:59 +0530

    Update PatientController.php

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -145,9 +145,9 @@ class PatientController extends Controller
                         $actionBtn .= '<a class="" href="' . $change_password_url . '"  title="Change Password" id="change_password_doctor" ><i class="fas fa-key"></i></a> ';
                     }
                     $checkConsentForm = ConsentAnswer::where('patient_id', $row->id)->count();
-                    $consentFormBtn = "btn btn-success";
+                    $consentFormBtn = "btn btn-danger";
                     if ($checkConsentForm != 0) {
-                        $consentFormBtn = "btn btn-danger";
+                        $consentFormBtn = "btn btn-success";
                     }
                     $actionBtn .= ' <a class="' . $consentFormBtn . '" href="' . $consentForm . '" target="_blank"   title="Consent Form"><i class="fa fa-wpforms" ></i></a>';
 
